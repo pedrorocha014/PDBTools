0979a6630c08f63307177d2e601249c220a7eaf2
pedrorocha <19pedrorocha96@gmail.com>
2022-12-12 10:34:17 -0300

    wip: add ANISOU and HETATM models

--- PDBTools/Models/PdbDataModel.cs
+++ PDBTools/Models/PdbDataModel.cs
@@ -33,11 +33,38 @@
     }
 
     public class Anisou{
-
+        public int Serial { get; set; }
+        public string Name { get; set; }
+        public string AltLoc { get; set; }
+        public string ResName { get; set; }
+        public string ChainID { get; set; }
+        public int ResSeq { get; set; }
+        public string ICode { get; set; }
+        public int U_00 { get; set; }
+        public int U_11 { get; set; }
+        public int U_22 { get; set; }
+        public int U_01 { get; set; }
+        public int U_02 { get; set; }
+        public int U_12 { get; set; }
+        public string Element { get; set; }
+        public string Charge { get; set; }
     }
 
     public class Hetatm{
-
+        public int Serial { get; set; }
+        public string Name { get; set; }
+        public string AltLoc { get; set; }
+        public string ResName { get; set; }
+        public string ChainID { get; set; }
+        public int ResSeq { get; set; }
+        public string ICode { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public float Occupancy { get; set; }
+        public float TempFactor { get; set; }
+        public string Element { get; set; }
+        public string Charge { get; set; }
     }
 
     public class Ter{
